453a552c50bbd43c12503f129c4b0cbcd6526155
dobreve <emil.dobrev@mail.schwarz>
2023-07-06 02:07:30 +0300

    Add test

--- src/test/java/emildobrev/ecommerce/store/integration/product/ProductControllerIntegrationTest.java
+++ src/test/java/emildobrev/ecommerce/store/integration/product/ProductControllerIntegrationTest.java
@@ -7,32 +7,28 @@ import emildobrev.ecommerce.store.product.ProductRepository;
 import emildobrev.ecommerce.store.product.ProductService;
 import emildobrev.ecommerce.store.product.ProductServiceImp;
 import emildobrev.ecommerce.store.product.dto.ProductCartDTO;
+import emildobrev.ecommerce.store.product.dto.ProductDTO;
 import emildobrev.ecommerce.store.product.dto.WishListResponse;
 import emildobrev.ecommerce.store.user.Role;
 import emildobrev.ecommerce.store.user.User;
 import emildobrev.ecommerce.store.user.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.modelmapper.ModelMapper;
 
 
 import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Optional;
+import java.util.*;
 
 import static com.mongodb.assertions.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 
-
-public class ProductControllerIntegrationTest {
+class ProductControllerIntegrationTest {
 
     @Mock
     private UserRepository userRepository;
@@ -43,12 +39,29 @@ public class ProductControllerIntegrationTest {
     @Mock
     private ModelMapper modelMapper;
 
+    @Captor
+    private ArgumentCaptor<Product> productCaptor;
+
     @InjectMocks
     private ProductServiceImp productService;
+    private User user;
 
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
+        user = User.builder()
+                .id("user_id")
+                .firstName("John")
+                .lastName("Doe")
+                .email("john.doe@example.com")
+                .password("password123")
+                .cart(new HashSet<>())
+                .roles(Arrays.asList(Role.USER))
+                .birthdate(new Date())
+                .coupons(new HashSet<>())
+                .wishList(new HashSet<>())
+                .build();
+
     }
 
     @Test
@@ -68,18 +81,6 @@ public class ProductControllerIntegrationTest {
         Product product = new Product();
         product.setId(id);
         product.setName("Sample Product");
-        User user = User.builder()
-                .id("user_id")
-                .firstName("John")
-                .lastName("Doe")
-                .email("john.doe@example.com")
-                .password("password123")
-                .cart(new HashSet<>())
-                .roles(Arrays.asList(Role.USER))
-                .birthdate(new Date())
-                .coupons(new HashSet<>())
-                .wishList(new HashSet<>())
-                .build();
 
         var wishlist = user.getWishList();
         wishlist.add(productWishlist);
@@ -102,5 +103,83 @@ public class ProductControllerIntegrationTest {
         Mockito.verify(userRepository, Mockito.times(1)).findByEmail(email);
         Mockito.verify(productRepository, Mockito.times(1)).findById(id);
         Mockito.verify(userRepository, Mockito.times(1)).save(user);
+    }
+
+    @Test
+    void testUpdateProduct() {
+        // Mock data
+        ProductDTO productDTO = ProductDTO.builder()
+                .id("1")
+                .price(BigDecimal.valueOf(2.55))
+                .description("")
+                .name("Product")
+                .category(Category.BOOKS)
+                .build();
+
+
+        Product existingProduct = new Product();
+        existingProduct.setId("1");
+        existingProduct.setPrice(BigDecimal.valueOf(5));
+        existingProduct.setDescription("");
+
+
+        Product updatedProduct = new Product();
+        updatedProduct.setId("1");
+        updatedProduct.setPrice(BigDecimal.valueOf(2.55));
+        updatedProduct.setDescription("");
+
+
+        when(productRepository.findById(productDTO.getId())).thenReturn(Optional.of(existingProduct));
+        when(productRepository.save(existingProduct)).thenReturn(updatedProduct);
+
+
+        when(modelMapper.map(updatedProduct, ProductDTO.class)).thenReturn(productDTO);
+
+
+        ProductDTO result = productService.updateProduct(productDTO);
+
+
+        verify(productRepository).findById(productDTO.getId());
+        verify(productRepository).save(existingProduct);
+
+
+        verify(modelMapper).map(updatedProduct, ProductDTO.class);
+
+        // Assertions
+        Assertions.assertEquals(productDTO.getId(), result.getId());
+        Assertions.assertEquals(productDTO.getPrice(), result.getPrice());
+        // Add more assertions for other properties if needed
+    }
+
+    @Test
+    void voteProduct_WithValidData_ShouldSaveProductWithUpdatedRating() {
+        // Arrange
+        String productId = "123";
+        String userEmail = "test@example.com";
+        Double userRating = 4.5;
+
+        Product product = new Product();
+        HashMap<String, Double> vote = new HashMap();
+        vote.put("test", 6.5);
+        product.setVotedUsers(vote);
+        user.setEmail(userEmail);
+
+        when(userRepository.findByEmail(userEmail)).thenReturn(Optional.of(user));
+        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
+
+
+        // Act
+        productService.voteProduct(productId, userRating, userEmail);
+
+        verify(productRepository, times(1)).save(productCaptor.capture());
+
+        // Assert
+        Product result = productCaptor.getValue();
+
+        Assertions.assertEquals(2, result.getVotedUsers().size());
+        Assertions.assertTrue(result.getVotedUsers().containsKey(user.getId()));
+        Assertions.assertEquals(userRating, result.getVotedUsers().get(user.getId()));
+        Assertions.assertEquals(result.getRating(), 5.5);
+
     }
 }
